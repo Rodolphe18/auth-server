afafff7bb11fea5db75bf81b64a6008ae84e11af
rfrancotte <rodolphe.francotte@webedia-group.com>
2025-05-20 11:47:16 +0200

    routing for registration, log in & authenticate created

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -2,6 +2,10 @@ package com.francotte
 
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
+import com.francotte.data.UserDataSource
+import com.francotte.security.hashing.HashingInterface
+import com.francotte.security.token.TokenConfig
+import com.francotte.security.token.TokenInterface
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
@@ -13,8 +17,11 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.slf4j.event.*
 
-fun Application.configureRouting() {
+fun Application.configureRouting(userDataSource: UserDataSource, hashingInterface: HashingInterface, tokenInterface: TokenInterface, tokenConfig: TokenConfig) {
     routing {
-
+        signIn(hashingInterface,userDataSource,tokenInterface,tokenConfig)
+        signUp(hashingInterface, userDataSource)
+        authenticate()
+        getSecretInfo()
     }
 }
